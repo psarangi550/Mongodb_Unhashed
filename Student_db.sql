94b295e0812a45e4f9652cc7a3878877c66b7cd6
pratik kumar sarangi <psarangi50@gmail.com>
2022-04-01 13:28:19 +0530

    All Friday Mongo commits

---
+++ Student_db.sql
@@ -0,0 +1,16 @@
+db.Students.insertOne({
+    name:"pratik",
+    rollno : 101,
+    marks :98
+})
+db.Students.insert({
+    name:"rika",
+    rollno : 102,
+    marks :99
+})
+
+show collections
+
+db.Students.find()
+
+db.DropDatabase()
